5532ed97396724d17783bf949cc2f9882c6b7ea8
Adedayo Oshinnaiye <oshinnaiye@gmail.com>
2016-04-30 08:36:03 +0100

    Completed Checkpoint 29

--- Gemfile
+++ Gemfile
@@ -22,7 +22,6 @@ source 'https://rubygems.org'
 
  gem 'sass-rails', '~> 5.0'
 
-
  gem 'uglifier', '>= 1.3.0'
 
  gem 'coffee-rails', '~> 4.1.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,3 @@
 class Post < ActiveRecord::Base
-<<<<<<< HEAD
-<<<<<<< HEAD
   has_many :comments
-=======
->>>>>>> chkpt_28_Models
-=======
->>>>>>> chkpt_28_Models
 end
