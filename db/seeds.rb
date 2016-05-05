6a497f6531a98e18d6b39286dd7bc4a2030561c6
Adedayo Oshinnaiye <oshinnaiye@gmail.com>
2016-05-05 04:35:52 +0100

    Completed Assign 30

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'random_data'
 
 50.times do
-    
+
     Post.create!(
 
       title: RandomData.random_sentence,
@@ -13,12 +13,45 @@ require 'random_data'
 
  100.times do
     Comment.create!(
-    
+
       post: posts.sample,
       body: RandomData.random_paragraph
- 
+
     )
  end
+
+#Trial 1
+2.times do
+    Post.create!(
+
+        title: RandomData.random_sentence,
+        body:  RandomData.random_paragraph
+        )
+    end
+
+    uniq_post = Post.all
+
+2.times do
+    Comment.create!(
+
+        post: uniq_post.sample,
+        body: RandomData.random_paragraph
+
+        )
+    end
+
+  #Trail 2
+  Post.create!(
+      title: "Why am I great?",
+      body:  "Because I speak therefore I am"
+      )
+
+  trial_post = Post.all
+
+  Comment.create!(
+      post: trial_post.sample,
+      body: "This is why I am Great"
+      )
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
